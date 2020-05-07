ab5b4a54b55903ad56691d73d627f1b0a523eafb
Andrey Khomutov <homutovan@live.com>
2020-05-07 16:10:21 +0700

    Solution HW 2.2(bjs)

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -5,13 +5,10 @@ function getAnimalSound(animal) {
 function getAverageMark(marks) {
     const reducer = (sumRate, currentRate) => +sumRate + +currentRate;
     return !marks.length ? 0: Math.round(marks.reduce(reducer) / marks.length);
-    // код для задачи №2 писать здесь
-    // return averageMark
 }
 
 function checkBirthday(birthday) {
-    let now = new Date();
-    console.log(now);
-    // код для задачи №3 писать здесь
-    return 5;
+    let now = +new Date(), userBirthday = +new Date(birthday);
+    let age = (now - userBirthday) / 365.25 / 24 / 60 / 60 / 1000;
+    return (age > 18) ? true : false;
 }
