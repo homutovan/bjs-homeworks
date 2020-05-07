87ea22f0e5193df13c89a164247e11dbbcb6d6b0
Andrey Khomutov <homutovan@live.com>
2020-05-07 15:27:19 +0700

    Solution HW 2.2 (bjs)

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,17 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    return animal ? animal.sound : null;
 }
 
 function getAverageMark(marks) {
+    const reducer = (sumRate, currentRate) => +sumRate + +currentRate;
+    return !marks.length ? 0: Math.round(marks.reduce(reducer) / marks.length);
     // код для задачи №2 писать здесь
     // return averageMark
 }
 
 function checkBirthday(birthday) {
+    let now = new Date();
+    console.log(now);
     // код для задачи №3 писать здесь
-    // return verdict
+    return 5;
 }
