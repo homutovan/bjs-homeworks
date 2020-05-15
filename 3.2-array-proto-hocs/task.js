e0228b676eb20c11863cf8bc87070882a6e09795
Andrey Khomutov <homutovan@live.com>
2020-05-15 23:05:53 +0700

    Fix bug

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -16,10 +16,6 @@ function sum(...args) {
 
 let compareArrays = (arr1, arr2) => arr1.length == arr2.length2 && arr1.every((elem, index) => elem === arr2[index]);
 
-let arr1 = [1, 2, 4], arr2 = [1, 2, 3, 4];
-
-console.log(compareArrays(arr1, arr2));
-
 function memorize(fn, limit) {
     let memory = [];
     return function inner(...args) {
@@ -51,30 +47,30 @@ let foo = memorize(sum, 1);
 
 
 
-// // Тестирование оптимизированной функции с различной глубиной кеша
+// Тестирование оптимизированной функции с различной глубиной кеша
 
-// sleepTime = 100;
+sleepTime = 100;
 
-// testCase(memorize(sum, 5), 'Ortimized function, limit = 5')
-// //Ortimized function, limit = 5: 506.349853515625ms
+testCase(memorize(sum, 5), 'Ortimized function, limit = 5')
+//Ortimized function, limit = 5: 506.349853515625ms
 
-// testCase(memorize(sum, 3), 'Ortimized function, limit = 3')
-// //Ortimized function, limit = 3: 50512.67919921875ms
+testCase(memorize(sum, 3), 'Ortimized function, limit = 3')
+//Ortimized function, limit = 3: 50512.67919921875ms
 
-// testCase(memorize(sum, 1), 'Ortimized function, limit = 3')
-// //Ortimized function, limit = 3: 70726.8017578125ms
+testCase(memorize(sum, 1), 'Ortimized function, limit = 3')
+//Ortimized function, limit = 3: 70726.8017578125ms
 
-// // Тестирование исходной функции
+// Тестирование исходной функции
 
-// testCase(sum, 'Original function, sleep = 100')
-// //Original function, sleep = 100: 91017.82299804688ms
+testCase(sum, 'Original function, sleep = 100')
+//Original function, sleep = 100: 91017.82299804688ms
 
-// sleepTime = 0;
+sleepTime = 0;
 
-// testCase(sum, 'Original function, sleep = 0')
-// //Original function, sleep = 0: 907.810791015625ms
+testCase(sum, 'Original function, sleep = 0')
+//Original function, sleep = 0: 907.810791015625ms
 
-// //По результатам тестирования видим, что скорость выполнения функции с кешированием
-// //существенно выше, чем скорость выполнения исходной функции, при этом, она очень сильно зависит от глубины кэша
+//По результатам тестирования видим, что скорость выполнения функции с кешированием
+//существенно выше, чем скорость выполнения исходной функции, при этом, она очень сильно зависит от глубины кэша
 
 
